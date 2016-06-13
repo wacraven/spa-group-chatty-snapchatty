f4b0f0c2424d724237e3f190b5392a77a732d2a6
Brian Doane <brianmdoane@gmail.com>
2016-06-13 13:15:14 -0500

    initial bones of buildDom.js

---
+++ js/iifeBuildDom.js
@@ -0,0 +1,20 @@
+var Chatty = ((originalChatty) => {
+
+	function buildDom(messagesArray) {
+	var messageString = "";
+	var timeStamp = Date.now();
+	// Need var for getElById("?");
+
+
+	// Loop through array and add JSON
+		for (var i = 0; i < messagesArray.length; i++) {
+			
+		}
+
+		// Set HTML to string
+
+	};
+
+	Chatty.loadMessage(buildDom);
+
+})(Chatty || {});
---
+++ js/iifeDeleteMsg.js
@@ -0,0 +1,5 @@
+var Chatty = ((originalChatty) => {
+
+
+
+})(Chatty || {});
